6ace54596430cd097a66fb377b3c739f2f030054
devsolary <mathewsolomon91@gmail.com>
2025-01-27 18:21:01 -0800

    add transparent bg

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,31 @@
 
+
+
 const Home = () => {
   return (
     <>
-    <div className="h-[100vh] w-[100vw] bg-home-bg px-20 py-10">
+    <div className="h-[100vh] w-[100vw] bg-home-bg px-[5vw] py-10" style={{
+      backgroundSize: "100% 100vh"
+    }}>
       <div className="grid grid-cols-2">
+        <div className="relative">
+          {/* <div className="absolute bg-gradient-to-r from-purple-600 to-yellow-500">h111</div> */}
+        <div className="overflow-hidden z-20 h-[450px] w-[38vw] rounded-3xl bg-white bg-opacity-30 backdrop-blur-md font-orbitron text-4xl p-10 font-semibold text-white">Innovative <br />solutions in
+        <br /> 
 
-        <div className="h-[646px] w-[614px] bg-gradient-to-b from-[#3D1B60] to-[#D9F0FF]">hello 1</div>
-        <div>hello 2</div>
+to drive growth and engagement</div>
+        </div>
+        
+        <div className="grid grid-cols-2 space-x-4 space-y-4">
+          <div className="bg-grid1_bg bg-center bg-cover bg-no-repeat mt-2" style={{
+            backgroundSize: "300px 220px"
+          }}></div>
+          <div className=" bg-white bg-opacity-30 backdrop-blur-md rounded-3xl"></div>
+          <div className=" bg-white bg-opacity-30 backdrop-blur-md rounded-3xl"></div>
+          <div className="bg-grid4_bg bg-center bg-cover bg-no-repeat mt-2 rounded-3xl" style={{
+            backgroundSize: "300px 220px"
+          }}></div>
+        </div>
       </div>
     </div>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,14 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        orbitron: ['Orbitron', 'sans-serif'],
+      },
       backgroundImage: {
         "header-bg": "url('src/images/Rectangle_6.png')",
-        "home-bg": "url('src/images/Hero Bg.png')"
+        "home-bg": "url('src/images/Hero Bg.png')",
+        "grid1_bg": "url('src/images/Frame_2.png')",
+        "grid4_bg": "url('src/images/image_1.png')",
       }
     },
   },
