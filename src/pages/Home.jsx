83a37537dad18f0bbfcf85374908b0ad7e11a3b8
devSolary <mathewsolomon91@gmail.com>
2025-02-04 17:49:48 +0100

    adjust home imagaes size

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -26,15 +26,15 @@ const Home = () => {
   return (
     <>
       <div
-        className="h-[100vh] w-[100vw] bg-home-bg px-[5vw] py-10"
+        className="h-[100vh] w-[100vw] bg-home-bg px-[10vw] py-10 bg-cover"
         style={{
           backgroundSize: "100% 100vh",
         }}
       >
-        <div className="grid grid-cols-2">
+        <div className="grid grid-cols-2 space-x-5">
           <div className="relative">
             {/* <div className="absolute bg-gradient-to-r from-purple-600 to-yellow-500">h111</div> */}
-            <div className="overflow-hidden z-20 h-[450px] w-[38vw] rounded-3xl bg-white bg-opacity-30 backdrop-blur-md font-orbitron text-4xl p-10 font-semibold text-white">
+            <div className="overflow-hidden z-20 h-[400px] w-[40vw] rounded-3xl bg-white bg-opacity-30 backdrop-blur-md font-orbitron text-4xl p-10 px-16 font-semibold text-white">
               Innovative <br />
               solutions in
               <br />
@@ -88,29 +88,33 @@ const Home = () => {
             
           </div>
 
-          <div className="grid grid-cols-2 space-x-4 space-y-4">
+          <div className="flex flex-col">
+            <div className="flex flex-row">
             <div
-              className="bg-grid1_bg bg-center bg-cover bg-no-repeat mt-4 min-h-52 max-h-52"
+              className="bg-grid1_bg bg-center bg-cover bg-no-repeat relative w-[250px] h-[190px] mr-3"
               style={{
-                backgroundSize: "300px 220px",
+                backgroundSize: "250px 190px",
               }}
             ></div>
-            <div className="flex flex-col min-h-48 max-h-52 text-white bg-white bg-opacity-30 backdrop-blur-md rounded-3xl px-6 pt-8 ">
-              <h1 className="font-orbitron text-2xl pb-2">Creative 
+            <div className="flex flex-col text-white bg-white bg-opacity-30 backdrop-blur-md rounded-3xl px-6 max-w-[230px] max-h-[170px] mt-2 pt-2">
+              <h1 className="font-orbitron text-xl pb-2">Creative 
               Solutions</h1>
-              <p>Turn visionary ideas into practical solutions that inspire and engage.</p>
+              <p className="text-sm">Turn visionary ideas into practical solutions that inspire and engage.</p>
             </div>
-            <div className=" bg-white bg-opacity-30 backdrop-blur-md text-white rounded-3xl min-h-52 max-h-52  px-6 pt-8">
-            <h1 className="font-orbitron text-2xl pb-2">Adaptable
+            </div>
+            <div className="flex flex-row mt-4">
+            <div className=" bg-white bg-opacity-30 backdrop-blur-md text-white rounded-3xl px-6 pt-2 max-w-[230px] max-h-[180px] ml-3 mt-2 mr-3">
+            <h1 className="font-orbitron text-xl pb-2 ">Adaptable
             Systems</h1>
-              <p>Enjoy flexible and scalable digital solutions designed to evolve with your business needs</p>
+              <p className="text-sm">Enjoy flexible and scalable digital solutions designed to evolve with your business needs</p>
             </div>
             <div
-              className="bg-grid4_bg bg-center bg-cover bg-no-repeat mt-2 rounded-3xl min-h-52 max-h-52"
+              className="bg-grid4_bg bg-center bg-cover bg-no-repeat w-[250px] h-[190px] mt-2"
               style={{
-                backgroundSize: "300px 220px",
+                backgroundSize: "250px 190px",
               }}
             ></div>
+            </div>
           </div>
         </div>
       </div>
