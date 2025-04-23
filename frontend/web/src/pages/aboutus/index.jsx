0035c6a0ca459c5e635a9088cfc5f12f806ff479
devSolary <mathewsolomon91@gmail.com>
2025-04-22 18:55:26 -0700

    create about page

--- frontend/web/src/pages/aboutus/index.jsx
+++ frontend/web/src/pages/aboutus/index.jsx
@@ -1,8 +1,20 @@
-
+import { Link } from "react-router-dom";
+import { FaGreaterThan } from "react-icons/fa6";
 
 const AboutUs = () => {
   return (
-    <div>AboutUs</div>
+    <div className="bg-[#00BCD4] px-4">
+      <div className="flex flex-row space-x-2 py-10 font-inter">
+        <Link to="/" className="text-white">Home</Link>
+        <FaGreaterThan className="text-white text-[10px] flex mt-2" />
+        <Link to="/about" className="text-[#AEFFA1]">About Us</Link>
+      </div>
+      <div>
+        <h1 className=" font-orbitron font-bold text-[#3D1B60] text-2xl my-4">About Us</h1>
+        <h4 className=" font-inter font-bold text-[#3D1B60] pb-5">Our Story</h4>
+        <p></p>
+      </div>
+    </div>
   )
 }
 
